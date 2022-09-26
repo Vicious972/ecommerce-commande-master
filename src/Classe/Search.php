dd4c8f16b307298f946ad4a7525de621aae0f7e5
Vicious972 <frostalethug@gmail.com>
2022-09-26 08:36:44 -0400

    css opacity change

---
+++ src/Classe/Search.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Classe;
+use App\Entity\Category;
+
+class Search
+{
+//     public function __toString()
+// {
+//  return '';
+// }
+
+    /** Je type ma variable 
+     * @var string
+     */
+    public $string= '';
+
+    /** Je type ma variable
+     * @var Category[]
+     */
+    public $categories = [];
+}
+
+?>
